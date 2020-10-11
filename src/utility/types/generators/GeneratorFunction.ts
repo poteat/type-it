ee3905b5e77407a117f4a430b37b333a0628692f
Michael Poteat <me@mpote.at>
2020-10-11 14:43:02 -0700

    Add docs and "type tests"

--- src/utility/types/generators/GeneratorFunction.ts
+++ src/utility/types/generators/GeneratorFunction.ts
@@ -1,1 +1,5 @@
+/**
+ * The runtime type value (which is the base constructor of all generators) that
+ * is used to specify the generator type.
+ */
 export const GeneratorFunction = function* () {}.constructor;

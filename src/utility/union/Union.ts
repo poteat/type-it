4c9d36e112552a6023808bf80b701afb86643190
Michael Poteat <me@mpote.at>
2020-10-11 22:26:47 -0700

    Finish docs on union typings

--- src/utility/union/Union.ts
+++ src/utility/union/Union.ts
@@ -1,3 +1,6 @@
-export function Union<T extends any[]>(...args: T) {
-  return new UnionType(...args);
+/**
+ * Represents a union type of two or more types.
+ */
+export function Union<T1, T2, T extends any[]>(arg0: T1, arg1: T2, ...args: T) {
+  return new UnionType(arg0, arg1, ...args);
 }
--- src/utility/union/type/UnionType.ts
+++ src/utility/union/type/UnionType.ts
@@ -1,3 +1,11 @@
+/**
+ * Runtime representation of a "union" type. Each subtype represents one the
+ * possible runtime types that can check. If none check, the over-all union
+ * type does not check.
+ *
+ * We represent this structure as a class, so we can identify an instance of it
+ * at runtime.
+ */
 class UnionType<T extends any[]> {
   subtypes: T;
 

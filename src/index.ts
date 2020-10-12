4bc584af9cad4d3cf6f4b4088730d1015d8a002e
Michael Poteat <me@mpote.at>
2020-10-11 23:11:47 -0700

    Add docs on unions

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export { typeIt } from "./utility/core/typeIt";
 
 export { GeneratorFunction } from "./utility/types/generators/GeneratorFunction";
 export { AsyncGeneratorFunction } from "./utility/types/generators/AsyncGeneratorFunction";
+
+export { Union } from "./utility/union/Union";
